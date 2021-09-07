93320be30beedff55b8516f1449e90fad3816cf7
sullayang(杨金伟) <sullayang@futunn.com>
2021-09-07 15:02:35 +0800

    提升概率

--- src/talent.js
+++ src/talent.js
@@ -61,9 +61,9 @@ class Talent {
                 if(!i && include) return include;
                 const gradeRandom = Math.random();
                 let grade;
-                if(gradeRandom>=0.111) grade = 0;
-                else if(gradeRandom>=0.011) grade = 1;
-                else if(gradeRandom>=0.001) grade = 2;
+                if(gradeRandom>=0.555) grade = 0;
+                else if(gradeRandom>=0.055) grade = 1;
+                else if(gradeRandom>=0.005) grade = 2;
                 else grade = 3;
 
                 while(talentList[grade].length == 0) grade--;
